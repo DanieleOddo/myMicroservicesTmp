6ad6145c3afa3f565a07495e6e3f579abf50e9ce
DanieleOddo <daniele.oddo@outlook.com>
2023-02-02 17:32:18 +0100

    [FEATURE] Create nations initial commit

---
+++ nations/src/test/java/org/oddodan/nations/NationsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oddodan.nations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
