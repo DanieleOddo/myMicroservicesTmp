e5b54a5e38c9ce325ba7e9598a618e4a23256d1b
Daniele Oddo <daniele.oddo@outlook.com>
2023-02-05 20:55:13 +0100

    Fix Test

--- nations/src/main/java/org/oddodan/nations/NationsApplication.java
+++ nations/src/main/java/org/oddodan/nations/NationsApplication.java
@@ -2,8 +2,9 @@ package org.oddodan.nations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class NationsApplication {
 
 	public static void main(String[] args) {
